ac16e11b0740c2eb9f0dfac7ca6ee3e7670031fa
Colin <colinzqy@gmail.com>
2024-10-19 23:09:08 +0100

    Changed sentiment labelling to discrete

--- src/Sentiment/sentiment.py
+++ src/Sentiment/sentiment.py
@@ -10,6 +10,5 @@ class SentimentModel:
     def label(self, text):
         # Label sentiment with value ranging from -1 to 1
         output = self.pipeline(text)
-        score = output[0]['score']
-        return score if output[0]['label'] == 'POSITIVE' else -1 * score
+        return 1 if output[0]['label'] == 'POSITIVE' else -1
     
